f81abf2b075b8b8b0c252d11ceda3b561a4c5409
Antonio Hernández <ahernandez@emergya.com>
2012-01-03 13:19:34 +0100

    Add README file and others.

---
+++ script/i18n.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+lang=$1
+appname="firstboot"
+
+if [ "" == "$lang" ]; then
+    lang="es"
+fi
+
+podir="../po"
+potfilesin="${podir}/POTFILES.in"
+potfile="${podir}/${appname}.pot"
+pofile="${podir}/${lang}.po"
+pofilemerged="${podir}/${lang}.merged.po"
+mofile="${podir}/${appname}.mo"
+
+find .. -type f -name "*.py" > $potfilesin
+
+xgettext --language=Python --keyword=_ --output=$potfile -f $potfilesin
+
+if [ ! -f $pofile ]; then
+
+    msginit --input=$potfile --locale=es_ES --output-file $pofile
+
+else
+
+    msgmerge $pofile $potfile > $pofilemerged
+    mv $pofilemerged $pofile
+
+fi
+
+sed -i s@^../@@g $potfilesin
